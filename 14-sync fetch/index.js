3be0c9c7f505e90140e65f126fbf0309b88ad4b9
HarrySystems <luizgdpsantos@gmail.com>
2017-11-22 20:38:37 -0200

    Update index.js

--- 14-sync fetch/index.js
+++ 14-sync fetch/index.js
@@ -1,6 +1,6 @@
 (async () => {
 	console.log("teste")
-	aait fetch("https://code.jquery.com/jquery-3.2.1.slim.js").then((result) => {
+	await fetch("https://code.jquery.com/jquery-3.2.1.slim.js").then((result) => {
 		console.log(result)
 	})
 	console.log("teste2")
